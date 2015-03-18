79322df347ffedd0f91c5592c441864e20f6e061
Miroslaw Karpiewski <mirek0922@gmail.com>
2015-03-18 21:26:00 +0100

    updated geometry

--- app/src/main/java/pl/wroc/pwr/indoorlocalizationtieto/Geometry/Geometry.java
+++ app/src/main/java/pl/wroc/pwr/indoorlocalizationtieto/Geometry/Geometry.java
@@ -1,11 +1,9 @@
 package pl.wroc.pwr.indoorlocalizationtieto.Geometry;
 
-import java.util.List;
-
 /**
  * Created by Mateusz on 2015-03-18.
  */
-abstract class Geometry {
+public abstract class Geometry {
 
     public abstract double CalculateLength();
 
