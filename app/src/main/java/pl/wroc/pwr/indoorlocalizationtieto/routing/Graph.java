88cb710f9b0d2208acadf5429362e1210cee5e94
doma93 <d.zieba93@gmail.com>
2015-03-19 22:21:31 +0100

    Added Graph

--- app/src/main/java/pl/wroc/pwr/indoorlocalizationtieto/routing/Graph.java
+++ app/src/main/java/pl/wroc/pwr/indoorlocalizationtieto/routing/Graph.java
@@ -1,6 +1,38 @@
 package pl.wroc.pwr.indoorlocalizationtieto.routing;
 
+import java.util.ArrayList;
+
 public class Graph {
-    Graph() {
+    ArrayList<Vertex> vertexes;
+    ArrayList<Edge> edges;
+
+    public Graph() {
     }
+
+    public void addEdge(Vertex vertex1, Vertex vertex2, int dist){
+        Edge edge = new Edge(vertex1,vertex2,dist);
+        edges.add(edge);
+        vertex1.addNeighbour(vertex2);
+        vertex2.addNeighbour(vertex1);
+    }
+
+    public void addVertex(Vertex vertexId){
+        vertexes.add(vertexId);
+    }
+
+    public int getNumberOfVertexes(){
+        return vertexes.size();
+    }
+    public int getNumberOfEdges(){
+        return edges.size();
+    }
+
+    public String toString(){
+        String str = new String();
+        for(Edge e: edges){
+            str += e.getVertex1Id() + "-" + e.getVertex2Id() + ":" + e.getDistance() + "\n";
+        }
+        return str;
+    }
+
 }
---
+++ app/src/main/java/pl/wroc/pwr/indoorlocalizationtieto/routing/Vertex.java
@@ -0,0 +1,26 @@
+package pl.wroc.pwr.indoorlocalizationtieto.routing;
+
+import java.util.ArrayList;
+
+public class Vertex {
+    private int id;
+    private ArrayList<Vertex> neighbours;
+
+    public Vertex(int id){
+        this.id = id;
+    }
+    public void addNeighbour(Vertex neighbour){
+        neighbours.add(neighbour);
+    }
+    public int getId(){
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Vertex) {
+            return this.id == ((Vertex) o).id;
+        }
+        return false;
+    }
+}
