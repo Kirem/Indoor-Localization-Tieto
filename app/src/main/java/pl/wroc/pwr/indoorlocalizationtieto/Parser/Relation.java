50f4f63f4d16b2aad348fb75819f127d2d3a3f75
PiroACC <piotr.minicki1@gmail.com>
2015-04-09 20:00:32 +0200

    Changed data fetching from OverpassAPI
    Added Relation fetching.
    
    Modified classes according to last patchset
    instructions.
    
    Deleted class
    -Members
    
    Change-Id: I72866660979b3577b9d4dea918135455241443a0

--- app/src/main/java/pl/wroc/pwr/indoorlocalizationtieto/Parser/Relation.java
+++ app/src/main/java/pl/wroc/pwr/indoorlocalizationtieto/Parser/Relation.java
@@ -1,9 +1,20 @@
 package pl.wroc.pwr.indoorlocalizationtieto.Parser;
 
-/**
- * Created by Mateusz on 2015-03-22.
- */
+import android.util.Pair;
 
-//TODO skonczyc pozniej, najpierw sparsowac Node oraz Way
-public class Relation {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Relation extends OSMElement {
+
+    private List<Pair<String, OSMElement>> members;
+
+    public Relation(String TYPE, long id, Map<String, String> tags, List<Pair<String, OSMElement>> members) {
+        super(TYPE, id, tags);
+        this.members = members;
+        for (Pair<String, OSMElement> osmElementPair : this.members) {
+            osmElementPair.second.addPartOf(this);
+        }
+    }
 }
